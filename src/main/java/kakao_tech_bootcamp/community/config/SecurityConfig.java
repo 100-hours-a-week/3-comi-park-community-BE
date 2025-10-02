4d20c8d4459f70d66d5df8f02bd03f1eec6a3cd4
kmi0817 <kmi0817@naver.com>
2025-10-02 23:46:56 +0900

    [feat] 비밀번호 암호화용 SecurityConfig 설정
    인증인가는 추후 추가 예정이므로 우선 모든 요청을 허용하도록 했음

---
+++ src/main/java/kakao_tech_bootcamp/community/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package kakao_tech_bootcamp.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())  // REST API 서버이므로 CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // TODO: 개발 편의상 모든 요청 허용. 추후 인증인가 기능 추가 필요
+                )
+                .httpBasic(basic -> basic.disable())  // REST API 서버임으로 HTTP Basic 비활성화
+                .build();
+    }
+}
