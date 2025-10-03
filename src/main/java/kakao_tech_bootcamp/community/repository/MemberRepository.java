a44275635b6f604de9a6fa7351a7b9d97aed8905
kmi0817 <kmi0817@naver.com>
2025-10-03 15:08:13 +0900

    [feat] 이메일로 회원 찾는 메서드 추가

--- src/main/java/kakao_tech_bootcamp/community/repository/MemberRepository.java
+++ src/main/java/kakao_tech_bootcamp/community/repository/MemberRepository.java
@@ -4,9 +4,13 @@ import kakao_tech_bootcamp.community.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     public boolean existsByEmail(String email);
 
     public boolean existsByNickname(String nickname);
+
+    public Optional<Member> findByEmail(String email);
 }
