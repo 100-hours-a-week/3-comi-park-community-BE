8da30b81415c81d8b6e1f40bf7887bb7878e3c18
kmi0817 <kmi0817@naver.com>
2025-10-03 21:05:21 +0900

    [fix] confirmedPassword에 @NotBlank 제거
    수정할 필드만 담는 PATCH 요청이므로 @NotBlank로 입력을 강제하는 게 맞지 않음

--- src/main/java/kakao_tech_bootcamp/community/dto/MemberUpdateRequestDto.java
+++ src/main/java/kakao_tech_bootcamp/community/dto/MemberUpdateRequestDto.java
@@ -1,6 +1,5 @@
 package kakao_tech_bootcamp.community.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import kakao_tech_bootcamp.community.entity.Image;
 import lombok.Getter;
@@ -10,7 +9,6 @@ public class MemberUpdateRequestDto {
     @Pattern(regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[~.!@#$%^&*()_\\-+=\\[\\]{}|\\\\;:'\",?/]).{8,20}$", message = "비밀번호 형식이 유효하지 않습니다")
     private String password;
 
-    @NotBlank(message = "비밀번호가 일치하지 않습니다")
     private String confirmedPassword;
 
     @Pattern(regexp = "^\\S{1,10}$", message = "닉네임 형식이 유효하지 않습니다")
