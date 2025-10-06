5186bfe46785296205459ec53366cb0523f435e8
kmi0817 <kmi0817@naver.com>
2025-10-07 01:24:05 +0900

    [chore] 컬럼명과 nullable, updatable false로 지정

--- src/main/java/kakao_tech_bootcamp/community/entity/MemberPostLikeId.java
+++ src/main/java/kakao_tech_bootcamp/community/entity/MemberPostLikeId.java
@@ -1,5 +1,6 @@
 package kakao_tech_bootcamp.community.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +8,9 @@ import lombok.Setter;
 @Embeddable
 @Getter @Setter
 public class MemberPostLikeId {
+    @Column(name = "post_id", nullable = false, updatable = false)
     private Integer postId;
+    @Column(name = "member_id", nullable = false, updatable = false)
     private Integer memberId;
 
     public MemberPostLikeId(Integer postId, Integer memberId) {
