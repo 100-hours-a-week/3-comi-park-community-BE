f30a3db4f1f6ab866157b286fb746bc615cad5d3
kmi0817 <kmi0817@naver.com>
2025-10-09 00:08:14 +0900

    [fix] 댓글 삭제 시 204 응답하도록 수정

--- src/main/java/kakao_tech_bootcamp/community/controller/CommentController.java
+++ src/main/java/kakao_tech_bootcamp/community/controller/CommentController.java
@@ -47,10 +47,10 @@ public class CommentController {
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<ApiResponse<Void>> removeComment(@CurrentMember AuthInfo authInfo,
+    public ResponseEntity<Void> removeComment(@CurrentMember AuthInfo authInfo,
                                                      @PathVariable("postId") Integer postId,
                                                      @PathVariable("commentId") Integer commentId) {
         commentService.removeComment(authInfo.getId(), postId, commentId);
-        return ResponseEntity.ok(new ApiResponse<>("ok", null));
+        return ResponseEntity.noContent().build();
     }
 }
