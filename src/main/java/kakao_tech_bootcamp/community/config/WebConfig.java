74c50fcc1fa869bfb6b6ff266df46db2e961fe3a
kmi0817 <kmi0817@naver.com>
2025-10-15 22:22:19 +0900

    [fix] 프론트엔드 서버 CORS 허용하도록 함
    CORS preflight 요청은 PUBLIC ENDPOINT로 처리

--- src/main/java/kakao_tech_bootcamp/community/config/WebConfig.java
+++ src/main/java/kakao_tech_bootcamp/community/config/WebConfig.java
@@ -5,6 +5,7 @@ import kakao_tech_bootcamp.community.common.CurrentMemberResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,5 +30,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentMemberResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowCredentials(true);
     }
 }
