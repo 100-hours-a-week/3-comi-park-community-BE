333ea22e8b6196a4c55177326205be60cb79f671
kmi0817 <kmi0817@naver.com>
2025-10-15 22:19:04 +0900

    [feat] ApiResponse에 success 필드 추가
    프론트 쪽에서 필요함

--- src/main/java/kakao_tech_bootcamp/community/common/ApiResponse.java
+++ src/main/java/kakao_tech_bootcamp/community/common/ApiResponse.java
@@ -6,46 +6,47 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ApiResponse<T> {
+    private boolean success;
     private String message;
     private T data;
 
     public static <T> ApiResponse<T> success()  {
-        return new ApiResponse<>(ApiMessage.SUCCESS.getMessage(), null);
+        return new ApiResponse<>(true, ApiMessage.SUCCESS.getMessage(), null);
     }
 
     public static <T> ApiResponse<T> success(T data)  {
-        return new ApiResponse<>(ApiMessage.SUCCESS.getMessage(), data);
+        return new ApiResponse<>(true, ApiMessage.SUCCESS.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> created(T data)  {
-        return new ApiResponse<>(ApiMessage.CREATED.getMessage(), data);
+        return new ApiResponse<>(true, ApiMessage.CREATED.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> modified(T data)  {
-        return new ApiResponse<>(ApiMessage.MODIFIED.getMessage(), data);
+        return new ApiResponse<>(true, ApiMessage.MODIFIED.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> removed(T data)  {
-        return new ApiResponse<>(ApiMessage.REMOVED.getMessage(), data);
+        return new ApiResponse<>(true, ApiMessage.REMOVED.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> badRequest(T data) {
-        return new ApiResponse<>(ApiMessage.BAD_REQUEST.getMessage(), data);
+        return new ApiResponse<>(false, ApiMessage.BAD_REQUEST.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> unauthorized(T data) {
-        return new ApiResponse<>(ApiMessage.UNAUTHORIZED.getMessage(), data);
+        return new ApiResponse<>(false, ApiMessage.UNAUTHORIZED.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> forbidden(T data) {
-        return new ApiResponse<>(ApiMessage.FORBIDDEN.getMessage(), data);
+        return new ApiResponse<>(false, ApiMessage.FORBIDDEN.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> notFound(T data) {
-        return new ApiResponse<>(ApiMessage.NOT_FOUND.getMessage(), data);
+        return new ApiResponse<>(false, ApiMessage.NOT_FOUND.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> conflict(T data) {
-        return new ApiResponse<>(ApiMessage.CONFLICT.getMessage(), data);
+        return new ApiResponse<>(false, ApiMessage.CONFLICT.getMessage(), data);
     }
 }
