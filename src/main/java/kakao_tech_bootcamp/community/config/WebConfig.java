8e4f102764b752962707b53a95be17026fafffae
kmi0817 <kmi0817@naver.com>
2025-10-27 19:41:55 +0900

    [feat] 인터셉터의 excludePathPatterns 추가 정의
    정적 파일 서빙과 타임리프 관련 경로를 추가했음

--- src/main/java/kakao_tech_bootcamp/community/config/WebConfig.java
+++ src/main/java/kakao_tech_bootcamp/community/config/WebConfig.java
@@ -3,7 +3,7 @@ package kakao_tech_bootcamp.community.config;
 import kakao_tech_bootcamp.community.common.AuthInterceptor;
 import kakao_tech_bootcamp.community.common.CurrentMemberResolver;
 import kakao_tech_bootcamp.community.common.StorageProperties;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,19 +15,17 @@ import java.io.File;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
     private final CurrentMemberResolver currentMemberResolver;
 
-    @Autowired
-    public WebConfig(AuthInterceptor authInterceptor, CurrentMemberResolver currentMemberResolver) {
-        this.authInterceptor = authInterceptor;
-        this.currentMemberResolver = currentMemberResolver;
-    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/s3/members/*","/**.css") // static files
+                .excludePathPatterns("/terms", "/privacy"); // thymeleaf
     }
 
     @Override
