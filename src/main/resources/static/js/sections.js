a9647a934272a4795418bdf6f6b263ca7bd7cfb7
kmi0817 <kmi0817@naver.com>
2025-11-02 16:23:09 +0900

    [feat] header 뒤로 가기 이벤트 등록 (document.referrer가 현 커뮤니티일 때만 뒤로 가기 지원)

--- src/main/resources/static/js/sections.js
+++ src/main/resources/static/js/sections.js
@@ -5,4 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainElement = bodyElement.querySelector('main');
 
     paintFooter(bodyElement, mainElement);
+
+    bodyElement.querySelector('.header-backward-btn').addEventListener('click', () => {
+        if (ALLOWED_ORIGINS.some(origin => document.referrer.includes(origin))) {
+            history.back();
+        }
+    });
 });
