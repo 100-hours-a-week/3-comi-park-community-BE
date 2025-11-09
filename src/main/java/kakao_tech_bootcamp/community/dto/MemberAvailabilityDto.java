c7bc0298247c03123af83a751b1d2fc64e88e576
kmi0817 <kmi0817@naver.com>
2025-11-09 22:24:45 +0900

    [chore] 응답 DTO가 아닌 곳에 BaseResponse 구현한 것 제외

--- src/main/java/kakao_tech_bootcamp/community/dto/MemberAvailabilityDto.java
+++ src/main/java/kakao_tech_bootcamp/community/dto/MemberAvailabilityDto.java
@@ -3,11 +3,10 @@ package kakao_tech_bootcamp.community.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import kakao_tech_bootcamp.community.common.response.BaseResponse;
 import lombok.Getter;
 
 @Getter
-public class MemberAvailabilityDto implements BaseResponse {
+public class MemberAvailabilityDto {
     @Email(message = "이메일 형식이 유효하지 않습니다")
     @Size(max = 50, message = "이메일 형식이 유효하지 않습니다")
     private String email;
