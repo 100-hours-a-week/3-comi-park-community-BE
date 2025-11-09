c8b17468faad86d5df4c8829acfeb23ff9d96a10
kmi0817 <kmi0817@naver.com>
2025-11-09 18:43:29 +0900

    [chore] 사용하지 않는 import 제거

--- src/main/java/kakao_tech_bootcamp/community/config/WebConfig.java
+++ src/main/java/kakao_tech_bootcamp/community/config/WebConfig.java
@@ -6,7 +6,6 @@ import kakao_tech_bootcamp.community.common.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
