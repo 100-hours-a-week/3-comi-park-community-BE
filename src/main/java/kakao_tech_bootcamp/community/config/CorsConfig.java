5351dd6c6fb2c6cd92086867cb8f9f1384196493
kmi0817 <kmi0817@naver.com>
2025-11-15 14:28:03 +0900

    [feat] CORS 정책 설정에 프로필 제한 삭제

--- src/main/java/kakao_tech_bootcamp/community/config/CorsConfig.java
+++ src/main/java/kakao_tech_bootcamp/community/config/CorsConfig.java
@@ -2,13 +2,11 @@ package kakao_tech_bootcamp.community.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("dev")
-public class DevCorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Value("${cors.allow-origins}")
     private String[] allowOrigins;
 
