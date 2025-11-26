f690a881847247841658db5ad65dffa5256b8f5a
kmi0817 <kmi0817@naver.com>
2025-11-26 13:16:41 +0900

    [revert] 이미지 업로드 방식은 멀티파트 하나로 고정

--- src/main/java/kakao_tech_bootcamp/community/controller/ImageController.java
+++ src/main/java/kakao_tech_bootcamp/community/controller/ImageController.java
@@ -4,10 +4,8 @@ import kakao_tech_bootcamp.community.common.annotation.CurrentMember;
 import kakao_tech_bootcamp.community.common.response.BaseResponse;
 import kakao_tech_bootcamp.community.common.response.CommonResponse;
 import kakao_tech_bootcamp.community.common.response.ResponseFactory;
-import kakao_tech_bootcamp.community.dto.response.ImageResponseDto;
 import kakao_tech_bootcamp.community.service.AuthInfo;
-import kakao_tech_bootcamp.community.service.ImageStorage;
-import kakao_tech_bootcamp.community.service.ImageStorageStrategyFactory;
+import kakao_tech_bootcamp.community.service.ImageService;
 import kakao_tech_bootcamp.community.utils.objectKeyStrategy.ImageCategory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,21 +16,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/images")
 @RequiredArgsConstructor
 public class ImageController {
-    private final ImageStorageStrategyFactory imageStorageStrategyFactory;
+    private final ImageService imageService;
 
     @PostMapping()
     public ResponseEntity<CommonResponse<BaseResponse>> saveMemberImage(@CurrentMember AuthInfo authInfo,
-                                                                        @RequestParam(value = "storage", required = false, defaultValue = "LOCAL") ImageStorage storage,
                                                                         @RequestParam("category") ImageCategory category,
-                                                                        @RequestParam(value = "file", required = false) MultipartFile file) {
-        ImageUploadRequest uploadRequest = ImageUploadRequest.builder()
-                .file(file)
-                .build();
-
-        ImageResponseDto imageResponseDto = imageStorageStrategyFactory
-                .getStrategy(storage)
-                .saveImage(category, uploadRequest);
-
-        return ResponseFactory.created(imageResponseDto);
+                                                                        @RequestParam(value = "file") MultipartFile file) {
+        return ResponseFactory.created(imageService.saveImage(category, file));
     }
 }
